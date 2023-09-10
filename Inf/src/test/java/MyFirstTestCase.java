92a33b0f44bb588083b13fa18a37b1f7b2228184
SNARETBOOK\Snaret <oleborn@mail.ru>
2023-09-10 12:22:38 +0500

    Готовый тест стенд

---
+++ Inf/src/test/java/MyFirstTestCase.java
@@ -0,0 +1,29 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+
+public class MyFirstTestCase {
+    private String downloadFolder;
+    private WebDriver driver;
+    @Test
+    public void openBrowser () {
+        WebDriverManager.chromedriver().setup();
+        HashMap<String, Object> chromePrefs = new HashMap<>();
+        chromePrefs.put("download.default_directory", this.downloadFolder);
+        ChromeOptions opt = new ChromeOptions();
+        opt.setExperimentalOption("prefs", chromePrefs);
+        opt.addArguments("start-maximized");
+        driver = new ChromeDriver(opt);
+        driver.get("https://vk.com");
+        driver.
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
